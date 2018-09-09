0bd86ec48cde527e2ba5a883fe95247ef7afc852
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-09 22:26:03 +0300

    reducer

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,17 +1,52 @@
-const intialState = {
+import {
+  ADD_PLACE,
+  DELETE_PLACE,
+  SELECT_PLACE,
+  DESELECT_PLACE
+} from "../actions/actionTypes";
+
+const initialState = {
   places: [],
   selectedPlace: null
-}
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case value:
-      
-      break;
-  
+    case ADD_PLACE:
+      return {
+        ...state,
+        places: state.places.concat({
+          key: Math.random(),
+          name: action.placeName,
+          image: {
+            uri:
+              "https://c1.staticflickr.com/5/4096/4744241983_34023bf303_b.jpg"
+          }
+        })
+      };
+    case DELETE_PLACE:
+      return {
+        ...state,
+        places: state.places.filter(place => {
+          return place.key !== state.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+    case SELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: state.places.find(place => {
+          return place.key === action.placeKey;
+        })
+      };
+    case DESELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: null
+      };
     default:
-      break;
+      return state;
   }
-}
+};
 
-export default reducer
+export default reducer;
