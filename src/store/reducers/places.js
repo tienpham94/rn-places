9a1dfd2b188c47c374e1ba8b02cd5fb5cd32952a
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-09 22:25:45 +0300

    Actions

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,9 +1,17 @@
 const intialState = {
-
+  places: [],
+  selectedPlace: null
 }
 
 const reducer = (state = initialState, action) => {
-
+  switch (action.type) {
+    case value:
+      
+      break;
+  
+    default:
+      break;
+  }
 }
 
 export default reducer
